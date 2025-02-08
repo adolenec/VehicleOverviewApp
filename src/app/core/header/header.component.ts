e4882112ff463df0fd061b994be886e05f8ac622
adolenec <andrija.dolenec23@gmail.com>
2025-02-08 14:48:12 +0100

    Implement header component

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppRouteTokens } from 'src/app/app-routing.module';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private readonly router = inject(Router);
+
+  public signOut() {
+    this.router.navigateByUrl(AppRouteTokens.AUTH);
+  }
+}

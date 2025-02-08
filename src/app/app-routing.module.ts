fee562ad126e0d1181b098feeb12776c9be3676a
adolenec <andrija.dolenec23@gmail.com>
2025-02-08 10:34:02 +0100

    Setup routing, layout and app structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VehiclesComponent } from './core/vehicle/vehicles.component';
 
-const routes: Routes = [];
+export enum AppRouteTokens {
+  AUTH = 'auth',
+  VEHICLES = 'vehicles',
+}
 
+const routes: Routes = [
+  { path: '', redirectTo: AppRouteTokens.AUTH, pathMatch: 'full' },
+  {
+    path: AppRouteTokens.AUTH,
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  { path: AppRouteTokens.VEHICLES, component: VehiclesComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VehicleService } from './core/vehicle/vehicle.service';
+import { VehiclesComponent } from './core/vehicle/vehicles.component';
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, VehiclesComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [VehicleService],
   bootstrap: [AppComponent],
